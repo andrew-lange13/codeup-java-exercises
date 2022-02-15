2fc10cec86d9a3c4f43739de3f72b252eda8cda9
Andrew Lange <andrew.lange13@gmail.com>
2022-02-15 15:17:00 -0600

    completes java console exercise

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,83 @@
+import java.util.Scanner;
+
+public class ConsoleExercises {
+    public static void main(String[] args){
+
+        //Output pi
+/*
+        double pi = 3.14159;
+        System.out.printf("The value of pi is approximately %.2f%s%n", pi, ".");
+*/
+
+        //add string from scanner
+
+   /*     Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter Something: ");
+        String userInput = scanner.nextLine();
+
+        System.out.println("You entered: --> \"" + userInput + "\" <--");*/
+
+        //enter integer to scanner
+
+        /*Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int userInput = scanner.nextInt();
+
+        System.out.println(userInput);*/
+
+        // enter 3 words in scanner
+
+        /*Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter 3 words: ");
+        String userInput1 = scanner.next();
+        String userInput2 = scanner.next();
+        String userInput3 = scanner.next();
+
+        System.out.printf(userInput1 + "%n" + userInput2 + "%n" + userInput3);*/
+
+        // enter sentence in scanner
+
+       /* Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a sentence: ");
+        String userInput = scanner.next();
+
+        System.out.println(userInput);*/
+
+        //enter sentence in scanner with nextLine
+
+   /*     Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a sentence: ");
+        String userInput = scanner.nextLine();
+
+        System.out.println(userInput);*/
+
+        // calculate perimeter and area of your room
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the length of your room: ");
+        String userLength = scanner.nextLine();
+
+        System.out.print("Enter the width of your room: ");
+        String userWidth = scanner.nextLine();
+
+        System.out.print("Enter the height of your room: ");
+        String userHeight = scanner.nextLine();
+
+
+        float userLengthInt = Float.parseFloat(userLength);
+        float userWidthInt = Float.parseFloat(userWidth);
+        float userHeightInt = Float.parseFloat(userHeight);
+
+        System.out.printf("The area of your room is: %.2f%s%n", (userLengthInt * userWidthInt), " square feet.");
+        System.out.printf("The perimeter of your room is: %.2f%s%n", ((userLengthInt * 2) + (userWidthInt * 2)), " feet.");
+        System.out.printf("The volume of your room is: %.2f%s%n", (userLengthInt * userWidthInt * userHeightInt), " cubic feet.");
+
+    }
+}
+
