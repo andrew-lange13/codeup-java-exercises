5c4c4c19f4ffcdd75882062c7cd96853410a57d3
Andrew Lange <andrew.lange13@gmail.com>
2022-02-15 16:04:07 -0600

    adds to java control flow exercise

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,74 @@
+public class ControlFlowExercises {
+    public static void main(String[] args){
+
+        // while loop to 15
+
+        /*int i = 5;
+        while(i <= 15){
+            System.out.print(i + " ");
+            i++;
+        }*/
+
+        //for loop to 15
+
+       /* for(int i = 5; i<=15; i++){
+            System.out.print(i + " ");
+        }*/
+
+        // do while to 100 by 2
+
+       /* int i = 0;
+        do{
+            System.out.println(i);
+            i += 2;
+        }while(i <= 100);*/
+
+        // for loop to 100 by 2
+
+        /*for(int i = 0; i <= 100; i+=2){
+            System.out.println(i);
+        }*/
+
+        // do while from 100 to -10 by 5
+
+        /*int i = 100;
+        do{
+            System.out.println(i);
+            i -= 5;
+        }while(i >= -10);*/
+
+        // for loop from 100 to -10 by 5
+
+//        for(int i = 100; i >= -10; i -=5){
+//            System.out.println(i);
+//        }
+
+        // do while squared to one million
+
+        /*long i = 2;
+        do{
+            System.out.println(i);
+            i *= i;
+        }while(i < 1000000);*/
+
+        // for loop squared to one million
+
+        /*for(long i = 2; i < 1000000; i *= i){
+            System.out.println(i);
+        }*/
+
+        //Fizzbuzz
+
+        /*for(int i = 1; i <= 100; i++){
+            if(i % 3 == 0 && i % 5 == 0){
+                System.out.println("FizzBuzz");
+            }else if(i % 5 == 0){
+                System.out.println("Buzz");
+            }else if(i % 3 == 0){
+                System.out.println("Fizz");
+            }else {
+                System.out.println(i);
+            }
+        }*/
+    }
+}
