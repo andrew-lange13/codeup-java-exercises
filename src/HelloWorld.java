7ae285c8cbb6309810c042b171e4d70ffd70c5d9
Andrew Lange <andrew.lange13@gmail.com>
2022-02-15 12:05:32 -0600

    completes second java exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -8,6 +8,28 @@ public class HelloWorld {
     //like the front door of a house, you have to go through the door to get to any of the other rooms
     public static void main (String[] args){
         System.out.print("Hello, ");
-        System.out.print("World!");
+        System.out.println("World!");
+        int myFavoriteNumber = 13;
+        System.out.println("My favorite number is " + myFavoriteNumber);
+        String myString = "I like Turtles!";
+        System.out.println(myString);
+        float myNumber = 3.14f;
+        System.out.println(myNumber);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+        System.out.println(x);
+        System.out.println(y);
     }
 }
