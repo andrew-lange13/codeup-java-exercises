00e9e844ca2de2967bfad44e8d87c135b123e0ec
Andrew Lange <andrew.lange13@gmail.com>
2022-02-15 11:29:34 -0600

    completes first java exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,13 @@
+// class -defining the class
+//HelloWorld matches the name of the .java file
+
 public class HelloWorld {
+
+    //method - similar to a function in javascript
+    //main method -> specific method in java that acts as our entry point when running java
+    //like the front door of a house, you have to go through the door to get to any of the other rooms
+    public static void main (String[] args){
+        System.out.print("Hello, ");
+        System.out.print("World!");
+    }
 }
