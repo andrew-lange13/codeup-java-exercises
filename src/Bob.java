02edbd0093dac4d5a15843cd0859988b75b00017
Andrew Lange <andrew.lange13@gmail.com>
2022-02-15 19:48:55 -0600

    adds bob conversation]

---
+++ src/Bob.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        boolean bobTalk = true;
+
+//        System.out.println(bobInput);
+        while (bobTalk) {
+            String bob = "Talk to Bob: ";
+            System.out.print(bob);
+            String bobInput = scanner.nextLine();
+            if (bobInput.isEmpty()) {
+                System.out.println("Fine. Be that way!");
+            } else if (bobInput.endsWith("?")) {
+                System.out.println("Sure.");
+            } else if (bobInput.endsWith("!")) {
+                System.out.println("Whoa, chill out!");
+            } else if (bobInput.equals("exit")) {
+                bobTalk = false;
+            } else {
+                System.out.println("Whatever.");
+            }
+        }
+    }
+}
