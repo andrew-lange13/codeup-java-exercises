137fe10ab8f501e4d2962ce4e6c58071b25ce080
Andrew Lange <andrew.lange13@gmail.com>
2022-02-20 14:59:55 -0600

    adds to input java exercise

--- src/util/Input.java
+++ src/util/Input.java
@@ -10,34 +10,70 @@ public class Input {
         this.scanner = new Scanner(System.in);
     }
 
-    public String getString(){
+    public String getString() {
         return getString("Enter a string: ");
     }
 
-    public String getString(String prompt){
+    public String getString(String prompt) {
         System.out.print(prompt);
         return this.scanner.nextLine();
     }
 
-    public boolean yesNo(){
+    public boolean yesNo() {
         return yesNo("Enter yes or no: ");
     }
 
-    public boolean yesNo(String prompt){
+    public boolean yesNo(String prompt) {
         System.out.print(prompt);
-        String userAnswer = this.scanner.nextLine();
+        String userAnswer = this.scanner.next();
         return userAnswer.equalsIgnoreCase("y") | userAnswer.equalsIgnoreCase("yes");
     }
 
-    public int getInt(){
+    public int getInt() {
         return getInt("Enter an integer: ");
     }
 
-    public int getInt(String prompt){
+    public int getInt(String prompt) {
         System.out.print(prompt);
         return this.scanner.nextInt();
     }
 
+    public int getInt(int min, int max) {
+        return getInt("Enter an integer between " + min + " and " + max + " ", min, max);
+    }
+
+    public int getInt(String prompt, int min, int max){
+        do {
+            System.out.print(prompt);
+            int userInt = this.scanner.nextInt();
+            if(userInt >= min & userInt <= max){
+                return userInt;
+            }
+        } while (true);
+    }
+
+    public double getDouble() {
+        return getInt("Enter a double: ");
+    }
+
+    public double getDouble(String prompt) {
+        System.out.print(prompt);
+        return this.scanner.nextDouble();
+    }
+
+    public double getDouble(double min, double max) {
+        return getDouble("Enter a double between " + min + " and " + max + "  ", min, max);
+    }
+
+    public double getDouble(String prompt, double min, double max){
+        do {
+            System.out.print(prompt);
+            double userDouble = this.scanner.nextDouble();
+            if(userDouble >= min & userDouble <= max){
+                return userDouble;
+            }
+        } while (true);
+    }
 
 
 }
---
+++ src/util/InputTest.java
@@ -0,0 +1,12 @@
+package util;
+
+public class InputTest {
+
+    public static void main(String[] args) {
+        Input test = new Input();
+//        test.getString();
+//        test.yesNo();
+//        test.getInt(7, 77);
+//        test.getDouble(7.77, 77.77);
+    }
+}
