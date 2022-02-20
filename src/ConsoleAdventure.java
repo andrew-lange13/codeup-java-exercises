2acd861c3ac2fab4da333e7ba5743c5566bad942
Andrew Lange <andrew.lange13@gmail.com>
2022-02-20 14:31:08 -0600

    adds ascii art to console adventure

--- src/ConsoleAdventure.java
+++ src/ConsoleAdventure.java
@@ -12,48 +12,53 @@ public class ConsoleAdventure {
 
         //create player and enemy variables
         String player;
+        String name;
         int playerHealth;
         int playerAttackPoints;
         int playerDefensePoints;
-        String enemy1;
         int enemy1Health;
         int enemy1AttackPoints;
         int enemy1DefensePoints;
 
 
         //start game prompt
-        System.out.print("Do you want to play a game? y or n:  ");
+        System.out.println("\n" +
+                " █████╗ ██████╗ ██╗   ██╗███████╗███╗   ██╗████████╗██╗   ██╗██████╗ ███████╗    ████████╗██╗███╗   ███╗███████╗    ██╗██╗\n" +
+                "██╔══██╗██╔══██╗██║   ██║██╔════╝████╗  ██║╚══██╔══╝██║   ██║██╔══██╗██╔════╝    ╚══██╔══╝██║████╗ ████║██╔════╝    ██║██║\n" +
+                "███████║██║  ██║██║   ██║█████╗  ██╔██╗ ██║   ██║   ██║   ██║██████╔╝█████╗         ██║   ██║██╔████╔██║█████╗      ██║██║\n" +
+                "██╔══██║██║  ██║╚██╗ ██╔╝██╔══╝  ██║╚██╗██║   ██║   ██║   ██║██╔══██╗██╔══╝         ██║   ██║██║╚██╔╝██║██╔══╝      ╚═╝╚═╝\n" +
+                "██║  ██║██████╔╝ ╚████╔╝ ███████╗██║ ╚████║   ██║   ╚██████╔╝██║  ██║███████╗       ██║   ██║██║ ╚═╝ ██║███████╗    ██╗██╗\n" +
+                "╚═╝  ╚═╝╚═════╝   ╚═══╝  ╚══════╝╚═╝  ╚═══╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚══════╝       ╚═╝   ╚═╝╚═╝     ╚═╝╚══════╝    ╚═╝╚═╝\n" +
+                "                                                                                                                          ");
+        System.out.print("Do you want to play a game?   ");
         String startPrompt = scanner.nextLine();
 
         // if/else to evaluate start game prompt
-        if(!startPrompt.equalsIgnoreCase("y")){
-            System.out.println("No game for you!");
-        } else{
-            System.out.println("Lets Begin!");
+        if(startPrompt.equalsIgnoreCase("yes")){
             // name the character
             System.out.print("What is your characters name? ");
             // assign name to player1
             player = scanner.nextLine();
-            playerHealth = 100;
-            playerAttackPoints = 1;
-            playerDefensePoints = 1;
-            System.out.printf("Welcome to the dungeon %s%s%n%n%s%d%s%d%s%d%s%n", player, "!", "Your health = ", playerHealth, " and you have ", playerAttackPoints, " attack points and ", playerDefensePoints, " defense points.");
+            System.out.printf("Welcome to the adventure %s%s%n", player, "!");
             System.out.print("You only make it 7 steps into the dungeon when a level 1 goblinator attacks!! Fight or Flight?? :  ");
             String fightFlight = scanner.nextLine();
             if (!fightFlight.equalsIgnoreCase("fight")){
                 System.out.println("You run away........Coward");
             } else {
                 System.out.println("Let the fight begin!");
-                enemy1 = "goblinator";
+                name = "goblinator";
                 enemy1Health = 5;
                 enemy1AttackPoints = 1;
                 enemy1DefensePoints = 1;
-                System.out.printf("%s%s%d%s%d%s%d%s%n", enemy1, " health = ", enemy1Health, " and it has ", enemy1AttackPoints, " attack points and ", enemy1DefensePoints, " defense points.");
+                System.out.printf("%s%s%d%s%d%s%d%s%n", name, " health = ", enemy1Health, " and it has ", enemy1AttackPoints, " attack points and ", enemy1DefensePoints, " defense points.");
                 System.out.println(player + " attacks first and rolls the dice.");
                 System.out.printf("%s%s%d%n", player, " rolls a ", randomNumber);
-                System.out.printf("%s%s%d%s%s%n", player, " does ", (randomNumber * playerAttackPoints), " damage to ", enemy1);
+                System.out.printf("%s%s%d%s%s%n", player, " does ", (randomNumber), " damage to ", name);
             }
 
+
+        } else{
+            System.out.println("No game for you");
         }
 
 
