28a5ecdd7f85a33091d820814b91f12f2fcd8635
Andrew Lange <andrew.lange13@gmail.com>
2022-02-23 14:52:09 -0600

    fixes server name generator formatting

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,16 +1,15 @@
-import java.util.Arrays;
 import java.util.Random;
 
 public class ServerNameGenerator {
+
+    static String[] adjectives = {"bored", "angry", "annoyed", "average", "frail", "jittery", "grumpy", "pleasant", "selfish", "quaint"};
+
+    static String[] nouns = {"taco", "basketball", "computer", "truck", "picture", "chair", "turtle", "fan", "robot", "goldfish"};
 
     public static String getRando(String[] array) {
         int rando = new Random().nextInt(array.length);
         return array[rando];
     }
-
-    static String[] adjectives = {"bored", "angry", "annoyed", "average", "frail", "jittery", "grumpy", "pleasant", "selfish", "quaint"};
-
-    static String[] nouns = {"taco", "basketball", "computer", "truck", "picture", "chair", "turtle", "fan", "robot", "goldfish"};
 
     public static void main(String[] args) {
 
@@ -18,7 +17,6 @@ public class ServerNameGenerator {
         String randoNoun = getRando(nouns);
         System.out.println("Here is your server name:");
         System.out.println(randoAdj + "-" + randoNoun);
-
 
     }
 }
