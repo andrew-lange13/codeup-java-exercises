3a801d0492dfd978edff9c32fc3718a7b0093f86
Andrew Lange <andrew.lange13@gmail.com>
2022-02-28 12:12:12 -0600

    adds interfaces and abstracts to the exercise

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -3,13 +3,26 @@ package shapes;
 public class ShapesTest {
 
     public static void main(String[] args) {
-        Rectangle box1 = new Rectangle(5, 4);
-        System.out.println("box1.getArea() = " + box1.getArea());
-        System.out.println("box1.getPerimeter() = " + box1.getPerimeter());
+//        Rectangle box1 = new Rectangle(5, 4);
+//        System.out.println("box1.getArea() = " + box1.getArea());
+//        System.out.println("box1.getPerimeter() = " + box1.getPerimeter());
+//
+//        Rectangle box2 = new Square(7);
+//        System.out.println("box2.getArea() = " + box2.getArea());
+//        System.out.println("box2.getPerimeter() = " + box2.getPerimeter());
 
-        Rectangle box2 = new Square(7);
-        System.out.println("box2.getArea() = " + box2.getArea());
-        System.out.println("box2.getPerimeter() = " + box2.getPerimeter());
+        Measurable myShape;
+
+        myShape = new Rectangle(7, 4);
+
+        System.out.println("myShape.getPerimeter() = " + myShape.getPerimeter());
+        System.out.println("myShape.getArea() = " + myShape.getArea());
+
+        myShape = new Square(5);
+
+        System.out.println("myShape.getPerimeter() = " + myShape.getPerimeter());
+        System.out.println("myShape.getArea() = " + myShape.getArea());
+
 
     }
 
