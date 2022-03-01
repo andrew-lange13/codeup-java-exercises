7d7425c50b0c2394b77fb8f2b5da4430936ac544
Andrew Lange <andrew.lange13@gmail.com>
2022-03-01 10:08:27 -0600

    adds methods from exercise

---
+++ src/grades/Student.java
@@ -0,0 +1,41 @@
+package grades;
+
+import java.util.ArrayList;
+
+public class Student {
+
+    private final String studentName;
+    private final ArrayList<Integer> studentGrades;
+
+    public Student(String studentName){
+        this.studentName = studentName;
+        this.studentGrades = new ArrayList<>();
+    }
+
+    // TODO: returns the student's name
+    public String getName(){
+        return studentName;
+    }
+    // TODO: adds the given grade to the grades property
+    public void addGrade(int grade){
+        studentGrades.add(grade);
+    }
+    // TODO; returns the average of the students grades
+    public int getGradeAverage(){
+        int gradeSum = 0;
+        for(int num : studentGrades){
+            gradeSum += num;
+        }
+        return gradeSum / studentGrades.size();
+    }
+
+
+    public static void main(String[] args) {
+
+        Student bob = new Student("bob");
+
+    }
+
+
+
+}
