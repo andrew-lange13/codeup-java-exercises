5980e6601f868a6d8f5e47f3b3ae2576db67cc3b
Andrew Lange <andrew.lange13@gmail.com>
2022-03-02 11:01:33 -0600

    adds more exercises to practice class

--- src/Practice.java
+++ src/Practice.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class Practice {
 
@@ -15,27 +15,70 @@ public class Practice {
 
 //    TODO: Write a program that creates an array, and then prints the sum of the even and odd integers in a new array [sumOdd, sumEven]
 
-    public static int[] sumStuff() {
+//    public static int[] sumStuff() {
+//
+//        int[] nums = {2, 4, 8, 5, 1, 2};
+//        int[] numsOutput = new int[2];
+//        int evenSum = 0;
+//        int oddSum = 0;
+//        for (int num : nums) {
+//            if (num % 2 == 0) {
+//                evenSum += num;
+//            } else {
+//                oddSum += num;
+//            }
+//        }
+//        numsOutput[0] = oddSum;
+//        numsOutput[1] = evenSum;
+//        return numsOutput;
+//    }
 
-        int[] nums = {2, 4, 8, 5, 1, 2};
-        int[] numsOutput = new int[2];
-        int evenSum = 0;
-        int oddSum = 0;
-        for (int num : nums) {
-            if (num % 2 == 0) {
-                evenSum += num;
-            } else {
-                oddSum += num;
-            }
-        }
-        numsOutput[0] = oddSum;
-        numsOutput[1] = evenSum;
-        return numsOutput;
-    }
+
+//    TODO: 1. Create a command line grocery list maker
+//
+//    - before prompting the user to enter each item, ask how many items will be
+//            entered
+//    - for each item, the user should be prompted to enter in the name, quantity,
+//    and category of each grocery item
+//    - Create a `GroceryItem` class from which to instantiate `GroceryItem`
+//    objects to store in an array
+//    - once the user has entered all items, print out all items grouped by
+//    category and alphabetized in each group
+
+//    public static void groceryList(){
+//        Scanner scan = new Scanner(System.in);
+//        System.out.print("How many items will be entered: ");
+//        int numItems = scan.nextInt();
+//
+//    }
+
+
+//
+//     TODO: 2. Build a command line Tic Tac Toe game.
+//
+//            - the game board should be a multi-dimensional array of Strings
+//    - be sure the board displays the column letter and row number:
+//
+//    A   B   C
+//            1  -O- --- ---
+//            2  --- -X- ---
+//            3  --- --- ---
+//
+//            - the "O" in the example above is at 1a and the "X" is at 2B
+//    - the winning three in a row should match should be drawn with stars instead
+//    of dashes:
+//
+//    A   B   C
+//            1  *O* *O* *O*
+//            2  --- -X- ---
+//            3  -X- --- ---
+//
+//            - player one and player two should alternate turns
+//    - be sure to display who's turn it is and who won
 
     public static void main(String[] args) {
 
-        System.out.println("sumStuff() = " + Arrays.toString(sumStuff()));
+//        System.out.println("sumStuff() = " + Arrays.toString(sumStuff()));
 
 
     }
