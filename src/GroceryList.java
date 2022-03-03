3061acad548e968707d47ce71d851cbb5e2fae82
Andrew Lange <andrew.lange13@gmail.com>
2022-03-03 16:56:01 -0600

    adds to grocery list exercise

--- src/GroceryList.java
+++ src/GroceryList.java
@@ -1,8 +1,41 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class GroceryList {
 
-    Scanner scan = new Scanner(System.in);
+    private String groceryItem;
+    private int quantity;
+
+    public GroceryList(String item, int num){
+        this.groceryItem = item;
+        this.quantity = num;
+    }
+
+    public static void listyList(){
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Would you like to make a grocery list");
+        String wannaList = scan.nextLine();
+        if (wannaList.equalsIgnoreCase("yes")){
+            ArrayList<String> groceries = new ArrayList<>();
+            System.out.println("1: Meat\n2: Fruits\n3: Vegetables\n4: Drinks");
+            System.out.print("Pick a category 1-4: ");
+            int listChoice = scan.nextInt();
+            if (listChoice == 1){
+                System.out.println("Meat");
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        listyList();
+
+
+
+    }
 
 
 
