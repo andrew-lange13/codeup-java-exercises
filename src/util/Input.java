a9b8ff60e4b86680f5c73e9aa8b9129b9befe4fb
Andrew Lange <andrew.lange13@gmail.com>
2022-03-07 15:49:14 -0600

    adds getBinary

--- src/util/Input.java
+++ src/util/Input.java
@@ -12,7 +12,7 @@ public class Input {
     }
 
     public String getString() {
-        return getString("Enter a string: ");
+        return this.scanner.next();
     }
 
     public String getString(String prompt) {
@@ -21,7 +21,8 @@ public class Input {
     }
 
     public boolean yesNo() {
-        return yesNo("Enter yes or no: ");
+        String userAnswer = this.scanner.next();
+        return userAnswer.equalsIgnoreCase("y") | userAnswer.equalsIgnoreCase("yes");
     }
 
     public boolean yesNo(String prompt) {
@@ -31,15 +32,14 @@ public class Input {
     }
 
     public int getInt() {
-        return getInt("Enter an integer: ");
+        return this.scanner.nextInt();
     }
 
     public int getInt(String prompt) {
         try {
             System.out.print(prompt);
             return Integer.parseInt(this.getString());
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             System.out.println("Couldn't parse that ish" + e.getMessage());
             e.printStackTrace();
         }
@@ -50,24 +50,24 @@ public class Input {
         return getInt("Enter an integer between " + min + " and " + max + " ", min, max);
     }
 
-    public int getInt(String prompt, int min, int max){
+    public int getInt(String prompt, int min, int max) {
         do {
             System.out.print(prompt);
             int userInt = this.scanner.nextInt();
-            if(userInt >= min & userInt <= max){
+            if (userInt >= min & userInt <= max) {
                 return userInt;
             }
         } while (true);
     }
 
     public double getDouble() {
-        return getInt("Enter a double: ");
+        return this.scanner.nextDouble();
     }
 
     public double getDouble(String prompt) {
         try {
             System.out.print(prompt);
-            return Double.parseDouble(getString("Enter a double"));
+            return Double.parseDouble(getString());
         } catch (Exception e) {
             System.out.println("Couldn't parse that ish" + e.getMessage());
             e.printStackTrace();
@@ -80,15 +80,20 @@ public class Input {
         return getDouble("Enter a double between " + min + " and " + max + "  ", min, max);
     }
 
-    public double getDouble(String prompt, double min, double max){
+    public double getDouble(String prompt, double min, double max) {
         do {
             System.out.print(prompt);
             double userDouble = this.scanner.nextDouble();
-            if(userDouble >= min & userDouble <= max){
+            if (userDouble >= min & userDouble <= max) {
                 return userDouble;
             }
         } while (true);
     }
 
+    public String getBinary(){
+        System.out.print("Enter a number: ");
+         int binaryGuy = this.scanner.nextInt();
+         return Integer.toBinaryString(binaryGuy);
+    }
 
 }
