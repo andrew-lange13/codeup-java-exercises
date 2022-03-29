02ba98dcb7736796abd745452b11e8d8c26ba4e7
Andrew Lange <andrew.lange13@gmail.com>
2022-03-29 14:44:57 -0500

    adds codewars practice

--- src/Practice.java
+++ src/Practice.java
@@ -187,6 +187,13 @@ public class Practice {
         return sum;
     }
 
+//    TODO: Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+    public static String even_or_odd(int number) {
+        if (number % 2 == 0) {
+            return "Even";
+        } else return "Odd";
+    }
 
     public static void main(String[] args) {
 
