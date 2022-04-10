16c2c6524530d00209f9d8574536400cfcd33a2e
Andrew Lange <andrew.lange13@gmail.com>
2022-04-10 11:57:23 -0500

    practice from codewars

--- src/Practice.java
+++ src/Practice.java
@@ -218,6 +218,20 @@ public class Practice {
         return yourPoints > (average / classPoints.length);
     }
 
+//    TODO: Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+
+    public static int quarterOf(int month) {
+        if (month < 4){
+            return 1;
+        }else if (month < 7){
+            return 2;
+        }else if (month < 10){
+            return 3;
+        }else{
+            return 4;
+        }
+    }
+
     public static void main(String[] args) {
 
 //        System.out.println("sumStuff() = " + Arrays.toString(sumStuff()));
